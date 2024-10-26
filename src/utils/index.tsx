7fec05410f07dbceb457c89f845dbeb34683f255
Ahmadbekov Abdulholiq <abdulholiq571@gmail.com>
2024-10-27 04:43:39 +0500

    add: suspens animated

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,13 +1,16 @@
 import { Suspense } from "react";
+import Logo from "@/assets/images/logo.svg";
 
-const Loading = ()=>{
-    return (
-        <div>Loading...</div>
-    )
-}
+const Loading = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-[#09090A]">
+      <img className="animate-ping" src={Logo} alt="" width={300} />
+    </div>
+  );
+};
 
-const SuspenseComponent = ({children}: {children: JSX.Element})=> {
-    return <Suspense fallback={<Loading/>}>{children}</Suspense>
-}
+const SuspenseComponent = ({ children }: { children: JSX.Element }) => {
+  return <Suspense fallback={<Loading />}>{children}</Suspense>;
+};
 
-export {Loading, SuspenseComponent}
+export { Loading, SuspenseComponent };
